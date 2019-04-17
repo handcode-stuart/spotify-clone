077066ec5e2f8d1c637c1d1ab8dde9f6a2c778de
Stuart Morris <admin@handcode.co.uk>
2019-04-17 23:21:18 +0100

    Search bar styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,10 @@
 import React, { Component } from "react";
 
-import Loading from "../components/Loading/Loading";
-
 class Home extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            loading: true,
-        };
-    }
-    componentDidMount() {
-        setTimeout(() => {
-            this.setState({ loading: false });
-        }, 500);
-    }
     render() {
         return (
             <React.Fragment>
-                {!this.state.loading ? (
-                    <div>
-                        <h1>Home</h1>
-                    </div>
-                ) : (
-                    <Loading />
-                )}
+                <h1>Home</h1>
             </React.Fragment>
         );
     }
